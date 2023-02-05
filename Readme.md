dd57ca2239a79fef718c39465b745596a713ebb1
KumarSampurn <kumarsampurn@outlook.com>
2023-02-05 16:32:00 +0530

    Added Basic OpenCv template

--- Readme.md
+++
@@ -1,2 +0,0 @@
-#Gesture Volume Control
-
---
+++ VolumeHandControl.py
@@ -0,0 +1,21 @@
+import cv2
+import time
+
+cap= cv2.VideoCapture(0)
+pTime=0
+cTime=0
+
+while True:
+    success,img = cap.read()
+    img = cv2.flip(img, 1)
+
+
+
+
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+    cv2.putText(img,str((int)(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
